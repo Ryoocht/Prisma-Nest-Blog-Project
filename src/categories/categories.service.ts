04a39051b523858d70d0f9993be02d6d45979b17
Ryochi <ryo1.t.zubu@gmail.com>
2023-01-03 13:23:14 +1000

    adding category

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private prismaService: PrismaService) {}
+  create(createCategoryDto: CreateCategoryDto) {
+    return this.prismaService
+  }
+
+  findAll() {
+    return `This action returns all categories`;
+  }
+
+  findOne(id: string) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} category`;
+  }
+}
