3ba57ec253ccaf742990bdb225f9dfb03c667621
Ryochi <ryo1.t.zubu@gmail.com>
2023-01-04 17:04:47 +1000

    added post service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, CategoriesModule],
+  imports: [PrismaModule, UsersModule, CategoriesModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
